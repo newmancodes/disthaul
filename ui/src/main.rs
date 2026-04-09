6a7990b268a3bee187911d92a621cf912f965afb
Stephen Newman <steve@newman.digital>
2026-04-09 10:06:08 +0100

    feat: Add App component.

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,8 +1,26 @@
 use leptos::prelude::*;
 
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    view! {
+        <button
+            on:click=move |_| set_count.set(count.get() + 1)
+        >
+            "Click me: "
+            {count}
+        </button>
+        <p>
+            "Double count: "
+            {move || count.get() * 2}
+        </p>
+    }
+}
+
 fn main() {
     console_error_panic_hook::set_once();
-    leptos::mount::mount_to_body(|| view! { <p>"Hello, world! Is everything okay?"</p> })
+    mount_to_body(App);
 }
 
 #[cfg(test)]
