15d885a12a8302924a4fcec890a5d61ff5229986
Stephen Newman <steve@newman.digital>
2026-04-09 09:08:52 +0100

    feat: Add Keycloak dependency to AppHost to handle auth.

--- apphost.cs
+++ apphost.cs
@@ -1,5 +1,9 @@
 #:sdk Aspire.AppHost.Sdk@13.2.2
+#:package Aspire.Hosting.Keycloak@13.2.2-preview.1.26207.2
 
 var builder = DistributedApplication.CreateBuilder(args);
+
+var keycloak = builder.AddKeycloak("keycloak")
+    .WithDataVolume();
 
 builder.Build().Run();
