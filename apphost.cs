e69edeabe01e091619d6dce39a76e3a07ea58cdd
Stephen Newman <steve@newman.digital>
2026-04-09 20:58:40 +0100

    feat: Add client management api, kafka, redis, postgres.

--- apphost.cs
+++ apphost.cs
@@ -1,11 +1,37 @@
-#:sdk Aspire.AppHost.Sdk@13.2.2
+﻿#:sdk Aspire.AppHost.Sdk@13.2.2
+#:package Aspire.Hosting.Kafka@13.2.2
 #:package Aspire.Hosting.Keycloak@13.2.2-preview.1.26207.2
+#:package Aspire.Hosting.PostgreSQL@13.2.2
+#:package Aspire.Hosting.Redis@13.2.2
 #:property NoWarn=ASPIRECERTIFICATES001
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 var keycloak = builder.AddKeycloak("keycloak")
     .WithDataVolume();
+
+var kafka = builder.AddKafka("kafka")
+    .WithDataVolume()
+    .WithKafkaUI();
+
+var redis = builder.AddRedis("redis")
+    .WithDataVolume();
+
+var postgres = builder.AddPostgres("postgres");
+var clientManagementDatabase = postgres.AddDatabase("client-management");
+
+var clientManagementApi = builder
+    .AddExecutable("client-management-api", "cargo", "./client-management", "run", "--package", "api")
+    .WithHttpEndpoint(name: "http", env: "PORT")
+    .WithOtlpExporter()
+    .WithReference(kafka)
+    .WithReference(keycloak)
+    .WithReference(clientManagementDatabase)
+    .WithReference(redis)
+    .WaitFor(kafka)
+    .WaitFor(keycloak)
+    .WaitFor(postgres)
+    .WaitFor(redis);
 
 var ui = builder
     .AddExecutable("ui", "trunk", "./ui", "serve", "--watch", ".")
@@ -19,6 +45,8 @@ var ui = builder
     })
     .WithOtlpExporter()
     .WithReference(keycloak)
-    .WaitFor(keycloak);
+    .WithEnvironment("CLIENT_MANAGEMENT_API_URL", clientManagementApi.GetEndpoint("http"))
+    .WaitFor(keycloak)
+    .WaitFor(clientManagementApi);
 
 builder.Build().Run();
